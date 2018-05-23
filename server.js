a401e54146b0b14a92f50a2fb52cc788f160bd6b
Jaruned Sanyamad <eye-130@hotmail.com>
2018-05-23 20:48:36 +0700

    add

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+let contacts = []
+
+app.use(bodyParser.json())
+app.use(cors())
+
+/// TODO: Develop GET /contacts
+app.get('/contacts',(req,res) => {
+    let newContact = [
+        { name: this.newContactName, phoneNumber: this.newContactPhoneNumber }
+    ]
+    res.status(201).json(contacts)
+})
+
+
+
+/// TODO: Develop POST /contacts
+
+
+
+app.listen(3000, () => {
+    console.log('API Server started at port 3000')
+})
